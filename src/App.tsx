193a81a1cfc3612562dbddd6fcc442827e700bbb
Keita Ito <eguchi@eguchi.cc>
2024-01-14 12:03:10 +0000

    タイマーを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,72 @@
+import {
+  Button,
+  Container,
+  Flex,
+  MantineProvider,
+  Paper,
+  Stack,
+  Text,
+} from "@mantine/core";
+import "@mantine/core/styles.css";
+import { useInterval } from "@mantine/hooks";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
+function getTimerText(seconds: number, radix: number) {
+  return ("000" + (seconds % radix ** 3).toString(radix)).slice(-3);
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const interval = useInterval(() => setSeconds((s) => s + 1), 1000);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <MantineProvider>
+      <Container>
+        <Stack align="center">
+          <Text size="lg" style={{ width: "100%" }}>
+            共通テスト2024 数学I・A 第4問
+          </Text>
+          <Flex gap="lg">
+            <Paper shadow="sm" p="sm" w={100}>
+              <Text ta="center" size="lg">
+                T3 <br />
+                <b>{getTimerText(seconds, 3)}</b>
+              </Text>
+            </Paper>
+            <Paper shadow="sm" p="sm" w={100}>
+              <Text ta="center" size="lg">
+                T4 <br />
+                <b>{getTimerText(seconds, 4)}</b>
+              </Text>
+            </Paper>
+            <Paper shadow="sm" p="sm" w={100}>
+              <Text ta="center" size="lg">
+                T6 <br />
+                <b>{getTimerText(seconds, 6)}</b>
+              </Text>
+            </Paper>
+          </Flex>
+          <Button
+            onClick={interval.toggle}
+            color={interval.active ? "red" : "teal"}
+          >
+            {interval.active ? "ストップ" : "スタート"}
+          </Button>
+          <Text c="dimmed">
+            Created by えぐち (@pro_uecer)
+            <br />
+            <a
+              href="https://github.com/eguchi1611/kyotsu-2024-timer"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "inherit" }}
+            >
+              ソースコード (GitHub)
+            </a>
+          </Text>
+        </Stack>
+      </Container>
+    </MantineProvider>
   );
 }
 
